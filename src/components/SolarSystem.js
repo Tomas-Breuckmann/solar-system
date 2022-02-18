e7a0f8240c8f64471528355ea1e434737ba30b8c
Tomas Breuckmann <tomas.yoga@yahoo.com>
2022-02-18 17:20:08 -0300

    criterio 6

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 const headline = 'Planetas';
+// console.log(planets);
 class SolarSystem extends React.Component {
   render() {
     return (
       <>
         <div data-testid="solar-system"> </div>
         <Title headline={ headline } />
+        {planets.map((planeta) => (
+          <div key={ planeta.name }>
+            <PlanetCard planetName={ planeta.name } planetImage={ planeta.image } />
+          </div>
+        ))}
       </>
     );
   }
